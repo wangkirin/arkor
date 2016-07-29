6b7caaf15b8cbca06a6846117e62ed9366e0fc96
juzhen <j00381620@china.huawei.com>
2016-07-29 17:17:06 +0800

    Add the API POST /internal/v1/dataserver to init ONE new data server.
    Currently the API accepts only one dataserver json object. For multiple
    objects, we hope the framework could do the binding&validation like this:
     - m.Post('PATH', binding.Bind([]models.DataServer{}), HANDLER)
    
    If the framework can't do this, we'll have to implement it in a custom
    middleware.

--- handler/inner/dataserver.go
+++ handler/inner/dataserver.go
@@ -8,6 +8,7 @@ import (
 	"gopkg.in/macaron.v1"
 
 	"github.com/containerops/arkor/models"
+	"github.com/containerops/arkor/utils"
 	"github.com/containerops/arkor/utils/db"
 )
 
@@ -38,6 +39,36 @@ func PutDataserverHandler(ctx *macaron.Context, req models.DataServer, log *logr
 		log.Println(err.Error())
 		return http.StatusInternalServerError, []byte(err.Error())
 	}
+
+	return http.StatusOK, nil
+}
+
+func AddDataserverHandler(ctx *macaron.Context, req models.DataServer, log *logrus.Logger) (int, []byte) {
+	now := time.Now()
+
+	ds := &models.DataServer{
+		ID:         utils.MD5ID(),
+		GroupID:    req.GroupID,
+		IP:         req.IP,
+		Port:       req.Port,
+		CreateTime: now,
+		UpdateTime: now,
+	}
+
+	// Query DataServer ID from SQL Database
+	if err := db.SQLDB.Create(ds); err != nil {
+		log.Println(err.Error())
+		return http.StatusInternalServerError, []byte(err.Error())
+	}
+
+	log.Println(ds)
+	// Save dataserver status to K/V Database
+	if err := db.KVDB.Create(ds); err != nil {
+		log.Println(err.Error())
+		return http.StatusInternalServerError, []byte(err.Error())
+	}
+
+	// Save dataserver status to SQL Database
 
 	return http.StatusOK, nil
 }
